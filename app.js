799ea3f319433d81584d4a2855fc30a4f40f08f6
Roman Rafikov <romanrara@yandex.ru>
2023-08-05 19:38:21 +0500

    13 sprint

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
-}).then(() => console.log('BD'));
+});
 
 app.use(bodyParser.json());
 
@@ -24,6 +24,4 @@ app.use((req, res) => {
   res.status(404).send({ message: 'Sorry cant find that!' });
 });
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000');
-});
+app.listen(3000);
